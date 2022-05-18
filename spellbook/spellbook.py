48eb5c07845fffdafc9c227f3f041f695c4e706a
hutuk <hurukpt@gmail.com>
2022-05-18 20:07:01 +0100

    mesage fixing

--- spellbook/spellbook.py
+++ spellbook/spellbook.py
@@ -153,23 +153,23 @@ class Spellbook(commands.Cog):
                         new_spell_list_valid.append(string.capwords(new_spell))
                         continue
 
-                        # save the valid spells, if any
+            # save the valid spells, if any
             if(len(new_spell_list_valid) > 0):
                 async with guild_group.Spell() as SpellGroup:
                     SpellGroup.extend(new_spell_list_valid)
                     SpellGroup.sort()
                     await self.sendDiscordMessage(ctx, ":sparkles: Success! :sparkles:",
-                                                  "You have scribed the following spells into your Spellbook:\n{}".format(", ".join(new_spell_list_valid)))
+                                                  "You have copied the following spell{} into your Spellbook:\n{}".format("spells" if(len(new_spell_list_valid) > 1) else "spell", ", ".join(new_spell_list_valid)))
 
-                    # send the duplicate spells, if any
+            # send the duplicate spells, if any
             if(len(new_spell_list_duplicate) > 0):
                 await self.sendDiscordMessage(ctx, ":coin: I'm saving you money! :coin:",
-                                              "You already had these spells in your Spellbook: \n{}".format(", ".join(new_spell_list_duplicate)))
+                                              "You already had {} in your Spellbook: \n{}".format("these spells" if(len(new_spell_list_duplicate) > 1) else "this spell", ", ".join(new_spell_list_duplicate)))
 
-                # send the invalid spells, if any
+            # send the invalid spells, if any
             if(len(new_spell_list_invalid) > 0):
                 await self.sendDiscordMessage(ctx, ":warning: Oh no! :warning:",
-                                              "The following spells are not valid:\n{}\nPlease make sure you spelled it right\nUsed ' and -'s correctly.\nPlease make sure your spell is in [this list](https://pastebin.com/YS7NmYqh)".format(", ".join(new_spell_list_invalid)))
+                                              "The following {} not valid:\n{}\nPlease make sure you spelled it right\nUsed ' and -'s correctly.\nPlease make sure your spell is in [this list](https://pastebin.com/YS7NmYqh)".format("spells are" if(len(new_spell_list_duplicate) > 1) else "spell is", ", ".join(new_spell_list_invalid)))
 
     @commands.group(name="remove")
     @commands.guild_only()
@@ -223,18 +223,17 @@ class Spellbook(commands.Cog):
                             new_spell_list_valid.remove(spell)
                             pass
                     SpellGroup.sort()
-                    await self.sendDiscordMessage(ctx, ":sob: Success :sob:", "You have ripped the pages of the following spells from your Spellbook:\n{}".format(
-                        ", ".join(new_spell_list_valid)))
+                    await self.sendDiscordMessage(ctx, ":sob: Success :sob:", "You have ripped the pages of the following {} from your Spellbook:\n{}".format("spells" if(len(new_spell_list_valid) > 1) else "spell", ", ".join(new_spell_list_valid)))
 
             # send the duplicate spells, if any
             if(len(new_spell_list_unlearned) > 0):
-                await self.sendDiscordMessage(ctx, ":question: Hmm? :question:", "You don't have these spells in your Spellbook:\n{}".format(
-                    ", ".join(new_spell_list_unlearned)))
+                await self.sendDiscordMessage(ctx, ":question: Hmm? :question:", "You don't have {} in your Spellbook:\n{}".format("these spells" if(len(new_spell_list_unlearned) > 1) else "this spell",
+                                                                                                                                   ", ".join(new_spell_list_unlearned)))
 
                 # send the invalid spells, if any
             if(len(new_spell_list_invalid) > 0):
                 await self.sendDiscordMessage(
-                    ctx, ":warning: Oh no! :warning:", "The following spells are not valid:\n{}\nPlease make sure you spelled it right\nUsed ' and -'s correctly.\nPlease make sure your spell is in [this list](https://pastebin.com/YS7NmYqh)".format(", ".join(new_spell_list_invalid)))
+                    ctx, ":warning: Oh no! :warning:", "The following {} not valid:\n{}\nPlease make sure you spelled it right\nUsed ' and -'s correctly.\nPlease make sure your spell is in [this list](https://pastebin.com/YS7NmYqh)".format("spells are" if (len(new_spell_list_invalid)) else "spell is", " ".join(new_spell_list_invalid)))
 
     @commands.command()
     @commands.guild_only()
