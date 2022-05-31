ca9ded6cda608b2a6897dc28a1982da5e7b8ca44
hutuk <hurukpt@gmail.com>
2022-05-31 21:05:02 +0100

    temp fix

--- spellbook/utils.py
+++ spellbook/utils.py
@@ -25,5 +25,5 @@ def listformatter(listtoformat):
 
 
 def processStringToList(spellList):
-    return sorted(set(string.capwords(str.lower(x.strip())).replace("Of", "of").replace("And", "and").replace("From", "from").replace("The", "the").replace("To", "to").replace("With", "with")
+    return sorted(set(string.capwords(str.lower(x.strip())).replace("Of", "of").replace("And", "and").replace("From", "from").replace("The", "the").replace("To", "to").replace("With", "with").replace("wither", "Wither").replace("deafness", "Deafness")
                       for x in spellList.split(",")))
